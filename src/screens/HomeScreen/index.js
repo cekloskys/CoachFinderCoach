2b8097c113f30d7825e5027974f6962baf20645f
Thomas Austin <austint@chc.edu>
2023-01-27 12:52:28 -0500

    Design of the home screen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { View, Text, TextInput, Pressable } from 'react-native';
 import styles from './styles';
 import SelectDropdown from 'react-native-select-dropdown';
 import { useNavigation } from '@react-navigation/native';
+import PhoneInput from 'react-native-phone-number-input';
 
 // const database = require
 
@@ -14,102 +15,128 @@ const HomeScreen = () => {
   const [dob, setDob] = useState('');
   const [gender, setGender] = useState('');
   const [sport, setSport] = useState('');
-  
-  const sports = [
-    'Lacrosse',
-    'Ice Hockey',
-    'Basketball',
-    'Soccer',
-    'Field Hockey',
-    'Track and Field',
-    'Cross Country',
-    'Softball',
-    'Baseball',
-    'Football',
-    'Volleyball',
-    'Tennis',
-    'Golf',
-    'Swimming',
-    'Diving',
-    'Gymnastics',
-    'Boxing',
-    'Wresting',
-    'Bowling',
-  ]; 
+  const [formattedValue, setFormattedValue] = useState("");
+  const [valid, setValid] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  const phoneInput = useRef<PhoneInput>(null);
 
-  const onSelectSport = () => { 
-    if (!sport){
-      alert('Please select a sport.');
-      return;
-    }
-    if (!phonenumber){
-      alert('Please enter a phone number');
-      return;
-    }
-    if (!dob){
-      alert('Please enter a date of birth in format MM-DD-YYYY.');
-      return;
-    }
-    if (!gender){
-      alert('Please enter a gender.');
-      return;
-    }
+const sports = [
+  'Lacrosse',
+  'Ice Hockey',
+  'Basketball',
+  'Soccer',
+  'Field Hockey',
+  'Track and Field',
+  'Cross Country',
+  'Softball',
+  'Baseball',
+  'Football',
+  'Volleyball',
+  'Tennis',
+  'Golf',
+  'Swimming',
+  'Diving',
+  'Gymnastics',
+  'Boxing',
+  'Wresting',
+  'Bowling',
+];
 
-    // alert(sport + ' Added!');
-    navigation.navigate('Address');
+const genders = [
+  'Male',
+  'Female',
+]
+
+const onSelectSport = () => {
+  if (!sport) {
+    alert('Please select a sport.');
+    return;
+  }
+  if (!phonenumber) {
+    alert('Please enter a phone number');
+    return;
+  }
+  if (!dob) {
+    alert('Please enter a date of birth in format MM-DD-YYYY.');
+    return;
+  }
+  if (!gender) {
+    alert('Please enter a gender.');
+    return;
   }
 
-    return (
-        <View style={styles.page}>
-          <SelectDropdown
-          data={sports}
-          defaultButtonText={'Select Sport'}
-          onSelect={(selectedItem, index) => {
-            setSport(selectedItem);
-          }}
-          buttonTextAfterSelection={(selectedItem, index) => {
-            return selectedItem;
-          }}
-          rowTextForSelection={(item, index) => {
-            return item;
-          }}
-          buttonStyle={styles.dropdownBtnStyle}
-          buttonTextStyle={styles.dropdownBtnStyle}
-          dropdownStyle={styles.dropdownDropdownStyle}
-          rowStyle={styles.dropdownRowStyle}
-          rowTextStyle={styles.dropdownRowTxtStyle}
-          />
-          <TextInput
-            value={phonenumber}
-            onChangeText={value => setPhonenumber(value)}
-            style={styles.phonenumber}
-            clearButtonMode={'while-editing'}
-            placeholder={'Phone Number'}
-            placeholderTextColor={'grey'}
-          />
-          <TextInput
-            value={dob}
-            onChangeText={value => setDob(value)}
-            style={styles.dob}
-            clearButtonMode={'while-editing'}
-            placeholder={'Date of birth'}
-            placeholderTextColor={'grey'}
-          />
-          <TextInput
-            value={gender}
-            onChangeText={value => setGender(value)}
-            style={styles.gender}
-            clearButtonMode={'while-editing'}
-            placeholder={'Gender'}
-            placeholderTextColor={'grey'}
-          />
-          <View style={styles.bottom}>
-              <Pressable style={styles.button} onPress={onSelectSport}>
-                  <Text style={styles.buttonText}>Next</Text>
-              </Pressable>
-          </View>
-        </View>
-      );
+  // alert(sport + ' Added!');
+  navigation.navigate('Address');
+}
+
+return (
+  <View style={styles.page}>
+    <SelectDropdown
+      data={sports}
+      defaultButtonText={'Select Sport'}
+      onSelect={(selectedItem, index) => {
+        setSport(selectedItem);
+      }}
+      buttonTextAfterSelection={(selectedItem, index) => {
+        return selectedItem;
+      }}
+      rowTextForSelection={(item, index) => {
+        return item;
+      }}
+      buttonStyle={styles.dropdownBtnStyle}
+      buttonTextStyle={styles.dropdownBtnTxtStyle}
+      dropdownStyle={styles.dropdownDropdownStyle}
+      rowStyle={styles.dropdownRowStyle}
+      rowTextStyle={styles.dropdownRowTxtStyle}
+    />
+    <PhoneInput
+      defaultValue={phonenumber}
+      defaultCode='US'
+      onChangeText={text => setPhonenumber(text)}
+      onChangeFormattedText={(text) => {
+        setFormattedValue(text);
+      }}
+      containerStyle={styles.dropdownBtnStyle}
+      textContainerStyle={styles.dropdownBtnTxtStyle}
+      ref={phoneInput}
+    />
+    <TextInput
+      value={dob}
+      onChangeText={value => setDob(value)}
+      style={styles.dob}
+      clearButtonMode={'while-editing'}
+      placeholder={'Date of birth'}
+      placeholderTextColor={'grey'}
+    />
+    <SelectDropdown
+      data={genders}
+      defaultButtonText={'Select Gender'}
+      onSelect={(selectedItem, index) => {
+        setGender(selectedItem);
+      }}
+      buttonTextAfterSelection={(selectedItem, index) => {
+        return selectedItem;
+      }}
+      rowTextForSelection={(item, index) => {
+        return item;
+      }}
+      buttonStyle={styles.dropdownBtnStyle}
+      buttonTextStyle={styles.dropdownBtnTxtStyle}
+      dropdownStyle={styles.dropdownDropdownStyle}
+      rowStyle={styles.dropdownRowStyle}
+      rowTextStyle={styles.dropdownRowTxtStyle}
+    />
+    <View style={styles.bottom}>
+      <Pressable style={styles.button} onPress={() => {
+        const checkValid = phoneInput.current?.isValidNumber(phonenumber);
+        setShowMessage(true);
+        setValid(checkValid ? checkValid : false);
+      }}>
+        <Text style={styles.buttonText}>Next</Text>
+      </Pressable>
+    </View>
+  </View>
+);
 }
 
 export default HomeScreen;
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,70 +1,66 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
-    page: {
-      padding: 10,
-    },
-    dropdownBtnStyle: {
-      width: '100%',
-      height: 50,
-      borderRadius: 8,
-      borderWidth: 1,
-      borderColor: 'grey',
-      backgroundColor: 'grey',
-    },
-    dropdownBtnTxtStyle: {
-      color: '#444',
-      textAlign: 'left',
-    },
-    dropdownDropdownStyle: {
-      backgroundColor: '#EFEFEF',
-    },
-    dropdownRowStyle: {
-      backgroundColor: '#EFEFEF',
-      orderBottomColor: '#C5C5C5',
-    },
-    dropdownRowTxtStyle: {
-      color: '#000',
-      textAlign: 'left',
-    },
-    button: {
-      backgroundColor: 'black',
-      margin: 2,
-      height: 30,
-      borderRadius: 0,
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'row',
-      zIndex: 100,
-      width: Dimensions.get('screen').width / 4,
-      marginHorizontal: 1,
-    },
-    buttonText: {
-      fontSize: 16,
-      fontWeight: 'bold',
-      color: 'white',
-    },
-    phonenumber: {
-      fontsize: 16,
-      marginBottom: 15,
-      borderBottomWidth: 1.0,
-      color: 'black',
-      borderColor: 'black',
-    },
-    dob: {
-      fontsize: 16,
-      marginBottom: 15,
-      borderBottomWidth: 1.0,
-      color: 'black',
-      borderColor: 'black',
-    },
-    gender: {
-      fonstsize: 16,
-      marginBottom: 15,
-      borderBottomWidth: 1.0,
-      color: 'black',
-      borderColor: 'black',
-    },
-  });
+  page: {
+    padding: 10,
+  },
+  dropdownBtnStyle: {
+    width: '100%',
+    height: 50,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    backgroundColor: 'lightgrey',
+    marginVertical: 15,
+  },
+  dropdownBtnTxtStyle: {
+    color: '#444',
+    textAlign: 'left',
+  },
+  dropdownDropdownStyle: {
+    borderRadius: 10,
+  },
+  dropdownRowStyle: {
+    backgroundColor: '#EFEFEF',
+  },
+  dropdownRowTxtStyle: {
+    color: '#000',
+    textAlign: 'left',
+  },
+  button: {
+    backgroundColor: 'lightgrey',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 15,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#444',
+  },
+  phonenumber: {
+    fontsize: 16,
+    marginVertical: 15,
+    borderWidth: 1.0,
+    borderColor: 'lightgrey',
+    padding: 10,
+    borderRadius: 10,
+  },
+  dob: {
+    fontsize: 16,
+    marginBottom: 15,
+    borderBottomWidth: 1.0,
+    color: 'black',
+    borderColor: 'black',
+  },
+  gender: {
+    fonstsize: 16,
+    marginBottom: 15,
+    borderBottomWidth: 1.0,
+    color: 'black',
+    borderColor: 'black',
+  },
+});
 
-  export default styles;
+export default styles;
